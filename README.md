b5d1073bfca4c97d41bb229a0b718f9cc3dd0e8f
ventu92 <ventury92_@hotmail.com>
2015-06-04 10:36:20 +0200

    css ok

--- README.md
+++ README.md
@@ -1,45 +1,12 @@
-# Práctica Validación de Formularios
-
-Se realiza una página web para una empresa que vende servicios de Internet a otras compañías.
-La empresa nos solicita un un formulario de contacto en el que recogeremos no sólo los datos de contacto, sino también los datos para facturación o acceso por Internet.
-
-**Información de Contacto:**
-- Nombre* 		
-- Apellidos* 	
-- Teléfono*
-- email* 	
-- repetir email 		
-- ¿Cómo nos has conocido? (Prefiero no contestar, Publicidad, Profesionales del sector, Internet, A través de un amigo o conocido )
-
-**Datos de facturación:**
-- Demandante (Particular o Empresa)*
-- CIF/NIF*
-- Nombre/Empresa*
-- Dirección*
-- CP*
-- Localidad*
-- Provincia*
-- País*
-- Código IBAN* 		
-- Forma de pago: Mensual, Trimestral, Anual* 	
-
-**Datos de acceso:**
-- usuario* 		
-- contraseña* 	
-- repetir contraseña 	
 
 **Validaciones y comportamiento del formulario:**
 Debemos hacer las siguientes validaciones en cliente antes de hacer el envío del formulario:
-- Todos los campos con * son requeridos	
-- Comprobaremos que el usuario no exista previamente en la bbdd (NIF o email, el CIF no es necesario).
-- Teléfono contendrá solo dígitos y un total de 9.	
-- CP tendrán que ser 5 digitos. Si son menos se completará con 0 a la izquierda.
-- email debe ser un correo electrónico válido (al menos en apariencia)
+- Comprobaremos que el usuario no exista previamente en la bbdd (NIF o email, el CIF no es necesario).	
+- CP . Si son menos se completará con 0 a la izquierda.
 - Por defecto estará marcado como demandante Particular y como Nombre (apartado Datos de facturación) la combinación de los campos Nombre y Apellidos de la información de contacto. Si el usuario selecciona como demandante Empresa, se borrará el contenido del campo “Nombre”, que pasará a llamarse “Empresa” para que el usuario lo rellene. 	
 - Los campos CIF/NIF y Nombre/Empresa adecuarán su label en función del demandante seleccionado.
 - Una vez insertado el código postal, se debe seleccionar la provincia y la localidad de forma automática. La localidad se rellenará con criterio libre.
-- El código IBAN debe ser válido.
-- El usuario debe tener al menos 4 caracteres, se rellenará de modo automático 	con el correo electrónico y no podrá ser modificado.
+- El usuario , se rellenará de modo automático 	con el correo electrónico y no podrá ser modificado.
 - La contraseña se debe forzar a que sea compleja.
 - Una vez pulsemos enviar en el formulario se mostrará un aviso al usuario de que se va a dar de alta y que se le pasará la primera cuota de 50€, 140€ o 550€ según corresponda (forma de pago). El usuario podrá cancelar la operación.
 
@@ -54,15 +21,5 @@ Debemos hacer las siguientes validaciones en cliente antes de hacer el envío de
     - jquery Complexify para la complejidad de la contraseña.
     - http://harvesthq.github.io/chosen/ para los select
 
-- Diseño **css propio o basado en un framework css** tipo Bootstrap o Foundation (nada de “reciclar” el de la práctica demo).  Si utilizas Bootstrap y no lo conoces, puedes apoyarte en [bootsnipp](http://bootsnipp.com/forms).
-
-- La práctica se debe colgar en github y debe partir de un fork de mi repositorio, para su seguimiento por mi parte. De cualquier manera en vuestro formulario deberá haber un enlace visible a vuestro repositorio en GitHub. Serán necesarios **al menos 5 commits en 3 días diferentes**.
 
 - El código html, css y js deberá ir en sus propios ficheros. Se debe disponer de **dos versiones del código, una de desarrollo y otra de producción** que tendrá todos los ficheros minified y concatenados, de modo que haya un único js para todo el código que no sea nuestro (jQuery, Validate…), un único css para todo el código que no sea nuestro (Bootstrap por ej.), y un único css o js para el código que hayamos hecho nosotros. Para realizar todo este proceso  y por temas de productividad y eficiencia **se recomienda encarecidamente el uso de Yeoman**. Para facilitar y "forzar" a ello, ya os proporciono la estructura de la aplicación con Yeoman (yo webapp). En infenlaces.com se colgará exclusivamente el código de producción de modo que pueda llegar al mismo navegando a partir de la [lista de usuarios](http://www.infenlaces.com).
-
-**Ayuda**:
-- Para la validación te puedes ayudar de la práctica solucionada de validación que os proporcioné.
-- [Capítulo I y II del libro Pro git](http://git-scm.com/book/es/v1), para entender el control de versiones git (aunque tan solo serán necesarios 4 o 5 comandos, viene bien para entender lo que hacemos).
-- [Artículo sobre uso de Yeoman](http://www.formandome.es/varios/yeoman-automatizar-el-flujo-de-trabajo-en-desarrollo-web/), donde además utilizo jQuery Validate.
-
-
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,332 @@
-console.log('\'Allo \'Allo!');
+'use strict'
+
+$.validator.addMethod("mobileES", function(phone_number, element) {
+    phone_number = phone_number.replace(/\(|\)|\s+|-/g, "");
+    return this.optional(element) || phone_number.length == 9 &&
+        phone_number.match(/^(6|8|9)[0-9]{8}$/);
+}, "Please specify");
+
+//*******************************************************************************************************
+
+$.validator.addMethod("postalCodeES", function(numero, element) {
+    return this.optional(element) || ($numero >= "1" && $numero <= "52999" && strlen($numero) == "5").test(value);
+}, "Please specify a valid postal code");
+
+//*******************************************************************************************************
+$.validator.addMethod("nifES", function(value) {
+    "use strict";
+
+    value = value.toUpperCase();
+
+    // Basic format test
+    if (!value.match("((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)")) {
+        return false;
+    }
+
+    // Test NIF
+    if (/^[0-9]{8}[A-Z]{1}$/.test(value)) {
+        return ("TRWAGMYFPDXBNJZSQVHLCKE".charAt(value.substring(8, 0) % 23) === value.charAt(8));
+    }
+    // Test specials NIF (starts with K, L or M)
+    if (/^[KLM]{1}/.test(value)) {
+        return (value[8] === String.fromCharCode(64));
+    }
+
+    return false;
+
+}, "Please specify a valid NIF number.");
+
+$.validator.addMethod("nowhitespace", function(value, element) {
+    return this.optional(element) || /^\S+$/i.test(value);
+}, "No white space please");
+
+//*******************************************************************************************************
+$.validator.addMethod("iban", function(value, element) {
+    // some quick simple tests to prevent needless work
+    if (this.optional(element)) {
+        return true;
+    }
+
+    // remove spaces and to upper case
+    var iban = value.replace(/ /g, "").toUpperCase(),
+        ibancheckdigits = "",
+        leadingZeroes = true,
+        cRest = "",
+        cOperator = "",
+        countrycode, ibancheck, charAt, cChar, bbanpattern, bbancountrypatterns, ibanregexp, i, p;
+
+    if (!(/^([a-zA-Z0-9]{4} ){2,8}[a-zA-Z0-9]{1,4}|[a-zA-Z0-9]{12,34}$/.test(iban))) {
+        return false;
+    }
+
+    // check the country code and find the country specific format
+    countrycode = iban.substring(0, 2);
+    bbancountrypatterns = {
+        "AL": "\\d{8}[\\dA-Z]{16}",
+        "AD": "\\d{8}[\\dA-Z]{12}",
+        "AT": "\\d{16}",
+        "AZ": "[\\dA-Z]{4}\\d{20}",
+        "BE": "\\d{12}",
+        "BH": "[A-Z]{4}[\\dA-Z]{14}",
+        "BA": "\\d{16}",
+        "BR": "\\d{23}[A-Z][\\dA-Z]",
+        "BG": "[A-Z]{4}\\d{6}[\\dA-Z]{8}",
+        "CR": "\\d{17}",
+        "HR": "\\d{17}",
+        "CY": "\\d{8}[\\dA-Z]{16}",
+        "CZ": "\\d{20}",
+        "DK": "\\d{14}",
+        "DO": "[A-Z]{4}\\d{20}",
+        "EE": "\\d{16}",
+        "FO": "\\d{14}",
+        "FI": "\\d{14}",
+        "FR": "\\d{10}[\\dA-Z]{11}\\d{2}",
+        "GE": "[\\dA-Z]{2}\\d{16}",
+        "DE": "\\d{18}",
+        "GI": "[A-Z]{4}[\\dA-Z]{15}",
+        "GR": "\\d{7}[\\dA-Z]{16}",
+        "GL": "\\d{14}",
+        "GT": "[\\dA-Z]{4}[\\dA-Z]{20}",
+        "HU": "\\d{24}",
+        "IS": "\\d{22}",
+        "IE": "[\\dA-Z]{4}\\d{14}",
+        "IL": "\\d{19}",
+        "IT": "[A-Z]\\d{10}[\\dA-Z]{12}",
+        "KZ": "\\d{3}[\\dA-Z]{13}",
+        "KW": "[A-Z]{4}[\\dA-Z]{22}",
+        "LV": "[A-Z]{4}[\\dA-Z]{13}",
+        "LB": "\\d{4}[\\dA-Z]{20}",
+        "LI": "\\d{5}[\\dA-Z]{12}",
+        "LT": "\\d{16}",
+        "LU": "\\d{3}[\\dA-Z]{13}",
+        "MK": "\\d{3}[\\dA-Z]{10}\\d{2}",
+        "MT": "[A-Z]{4}\\d{5}[\\dA-Z]{18}",
+        "MR": "\\d{23}",
+        "MU": "[A-Z]{4}\\d{19}[A-Z]{3}",
+        "MC": "\\d{10}[\\dA-Z]{11}\\d{2}",
+        "MD": "[\\dA-Z]{2}\\d{18}",
+        "ME": "\\d{18}",
+        "NL": "[A-Z]{4}\\d{10}",
+        "NO": "\\d{11}",
+        "PK": "[\\dA-Z]{4}\\d{16}",
+        "PS": "[\\dA-Z]{4}\\d{21}",
+        "PL": "\\d{24}",
+        "PT": "\\d{21}",
+        "RO": "[A-Z]{4}[\\dA-Z]{16}",
+        "SM": "[A-Z]\\d{10}[\\dA-Z]{12}",
+        "SA": "\\d{2}[\\dA-Z]{18}",
+        "RS": "\\d{18}",
+        "SK": "\\d{20}",
+        "SI": "\\d{15}",
+        "ES": "\\d{20}",
+        "SE": "\\d{20}",
+        "CH": "\\d{5}[\\dA-Z]{12}",
+        "TN": "\\d{20}",
+        "TR": "\\d{5}[\\dA-Z]{17}",
+        "AE": "\\d{3}\\d{16}",
+        "GB": "[A-Z]{4}\\d{14}",
+        "VG": "[\\dA-Z]{4}\\d{16}"
+    };
+
+    bbanpattern = bbancountrypatterns[countrycode];
+    // As new countries will start using IBAN in the
+    // future, we only check if the countrycode is known.
+    // This prevents false negatives, while almost all
+    // false positives introduced by this, will be caught
+    // by the checksum validation below anyway.
+    // Strict checking should return FALSE for unknown
+    // countries.
+    if (typeof bbanpattern !== "undefined") {
+        ibanregexp = new RegExp("^[A-Z]{2}\\d{2}" + bbanpattern + "$", "");
+        if (!(ibanregexp.test(iban))) {
+            return false; // invalid country specific format
+        }
+    }
+
+    // now check the checksum, first convert to digits
+    ibancheck = iban.substring(4, iban.length) + iban.substring(0, 4);
+    for (i = 0; i < ibancheck.length; i++) {
+        charAt = ibancheck.charAt(i);
+        if (charAt !== "0") {
+            leadingZeroes = false;
+        }
+        if (!leadingZeroes) {
+            ibancheckdigits += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(charAt);
+        }
+    }
+
+    // calculate the result of: ibancheckdigits % 97
+    for (p = 0; p < ibancheckdigits.length; p++) {
+        cChar = ibancheckdigits.charAt(p);
+        cOperator = "" + cRest + "" + cChar;
+        cRest = cOperator % 97;
+    }
+    return cRest === 1;
+}, "Please specify a valid IBAN");
+
+//*******************************************************************************************************
+$("#formulario").validate({
+    rules: {
+        nombre: {
+            lettersonly: true,
+            required: true
+        },
+        apellidos: {
+            required: true
+        },
+        telefono: {
+            required: true,
+            digits: true,
+            mobileES: true
+        },
+        email: {
+            required: true,
+            email: true,
+            minlength: 6
+                //remote : "php/validar_email_db.php"
+        },
+        emailConf: {
+            required: true,
+            equalTo: email
+        }
+    },
+    messages: {
+        nombre: {
+            lettersonly: "Solo letras",
+            required: "introduce tu nombre, obligatorio"
+        },
+        apellidos: {
+            required: "introduce tus apellidos, obligatorio"
+        },
+        telefono: {
+            required: "introduce tu telefono, obligatorio",
+            digits: "solo numeros por favor",
+            mobileES: "numero invalido"
+        },
+        email: {
+            required: "email es obligatorio",
+            email: "este formato no es valido",
+            minlength: "demasiado corto"
+
+        },
+        emailConf: {
+            required: "email confirmacion es obligatorio",
+            equalTo: "No coinciden los email"
+        }
+    }
+});
+$("#formulario2").validate({
+    rules: {
+        demandante: {
+            required: true
+        },
+        NIF: {
+            required: true,
+            nifES: true
+        },
+        empresa: {
+            required: true
+        },
+        direccion: {
+            required: true
+        },
+        cp: {
+            required: true,
+            postalCodeES: true
+        },
+        localidad: {
+            required: true
+        },
+        provincia: {
+            required: true
+        },
+        pais: {
+            required: true
+        },
+        IBAN: {
+            required: true,
+            iban: true
+        }
+
+    },
+    messages: {
+        demandante: {
+            required: "campo obligatorio"
+        },
+        NIF: {
+            required: "campo obligatorio",
+            nifES: "dni no valido"
+        },
+        empresa: {
+            required: "campo obligatorio"
+        },
+        direccion: {
+            required: "campo obligatorio"
+        },
+        cp: {
+            required: "campo obligatorio",
+            postalCodeES: "codigo postal no es correcto"
+        },
+        localidad: {
+            required: "campo obligatorio"
+        },
+        provincia: {
+            required: "campo obligatorio"
+        },
+        pais: {
+            required: "campo obligatorio"
+        },
+        IBAN: {
+            required: "campo obligatorio",
+            iban: "incorrecto"
+        }
+    }
+});
+
+$("#formulario3").validate({
+    rules: {
+        usuario: {
+            required: true,
+            minlength: 4
+        },
+        contraseña: {
+            required: true,
+        },
+        contraseña2: {
+            required: true,
+            equalTo: contraseña
+        }
+    },
+    messages: {
+        usuario: {
+            required: "campo obligatorio",
+            minlength: "minimo 4 letras"
+        },
+        contraseña: {
+            required: "campo obligatorio"
+        },
+        contraseña2: {
+            required: "campo obligatorio",
+            equalTo: "las contraseñas no coinciden"
+        }
+    }
+});
+
+
+$("#demandante").focusout(function() {
+    if($("#demandante").val()==2){
+        $("#nif").html("CIF:");
+        $("#demandante").html("Empresa");
+        $("#nif").attr({
+            "id":"cif",
+            "name": "cif"
+        });
+    }else{
+        $("#nif").html("NIF:");
+        $("#demandante").html("Nombre:");
+        $("#cif").attr({
+            "id":"nif",
+            "name": "nif"
+        });
+    }
+});
